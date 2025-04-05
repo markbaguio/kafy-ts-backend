11c1df2010c7a0a8c7bd2203c7fdf08880dedebd
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-04-05 16:20:39 +0800

    FEAT: add sign out feature with checker if no user is signed in.

--- src/handlers/authHandlers.ts
+++ src/handlers/authHandlers.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express-serve-static-core";
 import supabaseClient from "../utils/supabaseClient";
 import { ApiResponse, AuthenticationResponse } from "../utils/ApiReponse";
+import { Session, User } from "@supabase/supabase-js";
 
 export async function signUpNewUser(
   request: Request,
@@ -64,6 +65,52 @@ export async function signInUser(
       statusCode: 200,
       data: data,
       message: "User signed in successfully.",
+    };
+
+    response.status(res.statusCode).json(res);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function signOutUser(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  try {
+    const {
+      data: { user },
+    } = await supabaseClient.auth.getUser();
+
+    // check if user is signed in or not.
+    // if user is not signed in, return error response.
+    if (!user) {
+      const res: ApiResponse<null> = {
+        statusCode: 400,
+        data: null,
+        message: "No user currently signed in.",
+      };
+      response.status(400).json(res);
+      console.log(Error("No user currently signed in."));
+      return;
+    }
+
+    // sign out user.
+    const { error } = await supabaseClient.auth.signOut();
+
+    // if error is present, send it to the next middleware.
+    // send error to handleErrorMiddleware which will handle all errors.
+    if (error) {
+      next(error);
+      return;
+    }
+
+    // handle success
+    const res: ApiResponse<null> = {
+      statusCode: 200,
+      data: null,
+      message: "User signed out successfully.",
     };
 
     response.status(res.statusCode).json(res);
