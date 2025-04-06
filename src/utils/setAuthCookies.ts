aa5815f29edc434af75f840e4047bff75893e127
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-04-06 14:48:21 +0800

    WIP: add delete cookies before setting new ones.

--- src/utils/setAuthCookies.ts
+++ src/utils/setAuthCookies.ts
@@ -11,16 +11,23 @@ export function setAuthCookies({
   accessToken,
   refreshToken,
 }: setAuthCookiesParameters) {
+  const isProduction = process.env.ENVIRONMENT === "production";
+
+  //? Delete existing cookies before setting new ones.
+  response.clearCookie("access_token");
+  response.clearCookie("refresh_token");
+
+  //? Set cookies for access token and refresh token.
   response.cookie("access_token", accessToken, {
     httpOnly: true,
-    secure: process.env.ENVIRONMENT === "production",
+    secure: isProduction,
     sameSite: "strict",
     maxAge: 60 * 60 * 1000, // 1 hour in milliseconds
     path: "/", // cookie is valid for the entire domain
   });
   response.cookie("refresh_token", refreshToken, {
     httpOnly: true,
-    secure: process.env.ENVIRONMENT === "production",
+    secure: isProduction,
     sameSite: "strict",
     maxAge: 60 * 60 * 24 * 7 * 1000, // 7 days in milliseconds
   });
