300ef5f16c86c22f75a76b3ea28050d97976192d
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-04-06 14:29:13 +0800

    WIP: install cookie-parser. WIP: add setAuthCookies helper function.

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json()); // express.json() middleware to parse req.body to a usable json format.
+app.use(cookieParser()); // cookie-parser middleware to parse cookies from req.headers.
 
 app.use("/auth", authRouter);
 
@@ -19,3 +20,6 @@ app.use(errorHandlerMiddleware);
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+function cookieParser(): any {
+  throw new Error("Function not implemented.");
+}
---
+++ src/utils/setAuthCookies.ts
@@ -0,0 +1,27 @@
+import { Response } from "express-serve-static-core";
+
+type setAuthCookiesParameters = {
+  response: Response;
+  accessToken: string;
+  refreshToken: string;
+};
+
+export function setAuthCookies({
+  response,
+  accessToken,
+  refreshToken,
+}: setAuthCookiesParameters) {
+  response.cookie("access_token", accessToken, {
+    httpOnly: true,
+    secure: process.env.ENVIRONMENT === "production",
+    sameSite: "strict",
+    maxAge: 60 * 60 * 1000, // 1 hour in milliseconds
+    path: "/", // cookie is valid for the entire domain
+  });
+  response.cookie("refresh_token", refreshToken, {
+    httpOnly: true,
+    secure: process.env.ENVIRONMENT === "production",
+    sameSite: "strict",
+    maxAge: 60 * 60 * 24 * 7 * 1000, // 7 days in milliseconds
+  });
+}
