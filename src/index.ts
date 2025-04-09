67dd386b9d7cf87a0080d0d9f3aa4e4d572ccca8
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-04-09 16:47:59 +0800

    FIX: add cors to enable communication between domains.

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import authRouter from "./routes/auth";
 import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware";
 import cookieParser from "cookie-parser";
 import { AUTHROUTE } from "./utils/constants";
+import cors from "cors";
 
 const app = express();
 
@@ -13,6 +14,14 @@ const PORT = process.env.PORT;
 
 app.use(express.json()); // express.json() middleware to parse req.body to a usable json format.
 app.use(cookieParser()); // cookie-parser middleware to parse cookies from req.headers.
+// Enable CORS for all routes, allowing requests from your frontend (localhost:5173)
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow requests from your frontend
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allow the necessary methods
+    credentials: true, // Allow credentials (cookies) to be sent
+  })
+);
 
 app.use(AUTHROUTE, authRouter);
 
