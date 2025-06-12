88efa591ef1cd9e26b1d1effc23803593a1ff634
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-06-12 14:26:49 +0800

    WIP: add type guard for CustomApiError.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,6 @@
 import { ApiResponse } from "./ApiReponse";
 import { CustomErrorName } from "./constants";
+import { CustomApiError } from "./CustomApiError";
 
 export function isNoSessionError(
   response: unknown
@@ -9,5 +10,25 @@ export function isNoSessionError(
     response !== null &&
     typeof (response as ApiResponse<unknown>).errorName === "string" &&
     (response as ApiResponse<unknown>).errorName === CustomErrorName.No_Session
+  );
+}
+
+export function isCustomApiError(error: unknown): error is CustomApiError {
+  // return (
+  //   error instanceof CustomApiError &&
+  //   typeof error === "object" &&
+  //   error !== null
+  // );
+
+  return (
+    typeof error === "object" &&
+    error !== null &&
+    "statusCode" in error &&
+    "errorName" in error &&
+    "message" in error &&
+    typeof error.statusCode === "number" &&
+    typeof error.errorName === "string" &&
+    typeof error.message === "string" &&
+    error instanceof CustomApiError
   );
 }
