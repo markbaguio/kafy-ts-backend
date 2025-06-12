2bfcfa8edd639964694d7a2ad490711c30b00fca
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-06-12 14:23:29 +0800

    FIX: re-write get profile utility function.

--- src/database/Profile.ts
+++ src/database/Profile.ts
@@ -3,16 +3,31 @@ import { Tables } from "./supabase";
 
 export type Profile = Tables<"profiles">;
 
-export async function getProfile(id: string): Promise<Profile> {
+export async function getProfile(id: string): Promise<Profile | null> {
   const { data, error } = await supabaseClient
     .from("profiles")
     .select("*")
     .eq("id", id)
-    .limit(1);
+    .limit(1)
+    .maybeSingle();
 
   if (error) {
-    throw new Error("Failed to fetch profile");
+    throw error;
   }
 
-  return data[0];
+  return data;
 }
+
+// export async function getProfile(id: string): Promise<Profile> {
+//   const { data, error } = await supabaseClient
+//     .from("profiles")
+//     .select("*")
+//     .eq("id", id)
+//     .limit(1);
+
+//   if (error) {
+//     throw error;
+//   }
+
+//   return data[0];
+// }
