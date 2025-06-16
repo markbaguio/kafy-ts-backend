c9d78f953f1d0015b44aaab87033240225417117
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-06-16 14:43:06 +0800

    WIP: add new custom error name for PostgrestError.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,6 +4,7 @@ export const BASE_ROUTE = "/api";
 export enum CustomErrorName {
   No_Session = "No_Session",
   Profile_Not_Found = "Profile_Not_Found",
+  PostgrestError = "PostgrestError",
 }
 
 export enum CustomErrorMessage {
