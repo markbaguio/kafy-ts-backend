34abfe8fef4b5eb846a11fae98436110847e1304
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-06-16 15:01:29 +0800

    FEAT: add PostgrestError error handler. FEAT: show generic error response depending on the set NODE_ENV.

--- src/middlewares/errorHandlerMiddleware.ts
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -7,6 +7,10 @@ import {
   isNoSessionError,
   isPostgrestError,
 } from "../utils/utils";
+import { CustomErrorName } from "../utils/constants";
+import { PostgrestErrorType } from "../database/PostgrestErrorType";
+
+const IS_DEV = process.env.NODE_ENV === "development";
 
 export function errorHandlerMiddleware(
   error: unknown,
@@ -18,70 +22,90 @@ export function errorHandlerMiddleware(
   let res: ApiResponse<null> = {
     statusCode: 500,
     message: "An unexpected error occurred.",
-    // errorName: "Error",
-    // errorDetails: null,
   };
 
   //? Handle ZodError.
   if (error instanceof ZodError) {
     res.statusCode = 422;
     res.errorName = error.name;
-    res.message = "Validation Error";
-    res.errorDetails = error.flatten();
-    // res.error = error.format();
+    if (IS_DEV) {
+      res.message = "Validation Error";
+      res.errorDetails = error.flatten();
+    }
     console.error(error);
   }
   //? Handle Supabase Auth Error(e.g., incorrect login, registration issues)
   else if (isAuthApiError(error)) {
     res.statusCode = error.status;
     res.errorName = error.name;
-    res.message = error.message;
-    res.errorDetails = error;
+    if (IS_DEV) {
+      res.message = error.message;
+      res.errorDetails = error;
+    }
     console.error(error);
   } else if (isAuthError(error)) {
     res.statusCode = error.status ?? 500;
     res.errorName = error.name;
-    res.message = error.message;
-    res.errorDetails = error;
+    if (IS_DEV) {
+      res.message = error.message;
+      res.errorDetails = error;
+    }
     console.error(error);
   } else if (isCustomApiError(error)) {
     res.statusCode = error.statusCode;
     res.errorName = error.errorName;
-    res.message = error.message;
-    // res.errorDetails = error.stack;
+    if (IS_DEV) {
+      res.message = error.message;
+      // res.errorDetails = error.stack;
+    }
     console.error(error);
   } else if (isPostgrestError(error)) {
-    res.statusCode = parseInt(error.code);
+    res.statusCode = 500;
+    res.errorName = CustomErrorName.PostgrestError;
     res.message = error.message;
-    res.errorDetails = error.details;
+    if (IS_DEV) {
+      res.errorDetails = {
+        code: error.code,
+        details: error.details,
+        hint: error.hint,
+        message: error.message,
+      } satisfies PostgrestErrorType;
+    }
     console.error(error);
   }
   //? Handle general errors(like internal server errors)
   else if (error instanceof Error) {
     res.statusCode = 500; //? 500 for Internal Server Error.
     res.errorName = error.name;
-    res.message = error.message;
-    res.errorDetails = {
-      name: error.name,
-      message: error.message,
-      stack: error.stack,
-    };
+    if (IS_DEV) {
+      res.message = error.message;
+      res.errorDetails = {
+        name: error.name,
+        message: error.message,
+        stack: error.stack,
+      };
+    }
     console.error(error);
   }
   //? Handle if there is no session or no user signed in
   else if (isNoSessionError(error)) {
     res.statusCode = 400;
-    res.message = error.message;
     res.errorName = error.errorName;
-    res.errorDetails = error.errorDetails;
+    if (IS_DEV) {
+      res.message = error.message;
+      res.errorDetails = error.errorDetails;
+    }
     console.error(Error(error.message));
   }
   //? In case of unknown error
   else {
     res.statusCode = 500;
-    res.message = "An unknown error occurred.";
     res.errorName = "Error";
-    res.errorDetails = { error: "Unkown Error" };
+    if (IS_DEV) {
+      res.message = "An unknown error occurred.";
+      res.errorDetails = { error: "Unkown Error" };
+    }
+    console.error(error);
   }
 
   response.status(res.statusCode).json(res);
