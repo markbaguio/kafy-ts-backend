e6e2ef0d77a7776404b026acbecb8160f1fc6116
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-07-02 15:31:26 +0800

    WIP: add items per page constant.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,8 @@
 export const AUTHROUTE = "/auth";
 export const BASE_ROUTE = "/api";
 
+export const ITEMS_PER_PAGE = 10;
+
 export enum CustomErrorName {
   No_Session = "No_Session",
   Profile_Not_Found = "Profile_Not_Found",
