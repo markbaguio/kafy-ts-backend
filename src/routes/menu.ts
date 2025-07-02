3651f580b89629ce009042f318ccfe80b2f79c75
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-07-02 13:56:51 +0800

    WIP: add route paramenter for menu page.

--- src/routes/menu.ts
+++ src/routes/menu.ts
@@ -3,6 +3,6 @@ import { getMenu } from "../handlers/menuHandlers";
 
 const menuRouter = Router();
 
-menuRouter.get("/menu", getMenu);
+menuRouter.get("/menu/:page", getMenu);
 
 export default menuRouter;
