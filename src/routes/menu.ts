a3de5bf501d096ae54aee9837d5f8626087fd185
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-07-22 15:10:30 +0800

    wip(menu.ts): add route and route parameter for product detail.

--- src/routes/menu.ts
+++ src/routes/menu.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getMenu } from "../handlers/menuHandlers";
+import { getMenu, getProduct } from "../handlers/menuHandlers";
 
 const menuRouter = Router();
 
 menuRouter.get("/menu", getMenu);
+
+menuRouter.get("/menu/:product_id", getProduct);
 
 export default menuRouter;
