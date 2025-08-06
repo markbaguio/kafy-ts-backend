bd422189c31963f33bb99b77ff908db9bcd09f6b
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-08-06 13:51:48 +0800

    wip(constants.ts): add free shipping threshold for determinig if the order will be free shipping.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,6 +3,8 @@ export const BASE_ROUTE = "/api";
 
 export const ITEMS_PER_PAGE = 10;
 
+export const FREE_SHIPPING_THRESHOLD = 450;
+
 export enum CustomErrorName {
   No_Session = "No_Session",
   Profile_Not_Found = "Profile_Not_Found",
