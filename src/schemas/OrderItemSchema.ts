b68690774ade815b1c273c920bb382414fe65158
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-08-20 13:37:54 +0800

    wip(OrderItemSchema.ts): add Order Item schema

---
+++ src/schemas/OrderItemSchema.ts
@@ -0,0 +1,12 @@
+import z from "zod";
+
+export const OrderItemSchema = z.object({
+  id: z.number(),
+  created_at: z.string().datetime(),
+  product_id: z.number(),
+  product_name: z.string(),
+  price_at_purchase: z.number().nonnegative(),
+  order_id: z.number(),
+  quantity: z.number().min(1),
+  product_size: z.enum(["S", "M", "L"]),
+});
