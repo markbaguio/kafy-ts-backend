d0f4caa36bdff9ed9747175736c3c93d8106cdfa
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-08-20 13:07:56 +0800

    wip(order.ts): add orders route.

--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { postOrder } from "../handlers/ordersHandlers";
+import { getOrders, postOrder } from "../handlers/ordersHandlers";
 
 const orderRouter = Router();
 
 orderRouter.post("/order", postOrder);
+orderRouter.get("/orders", getOrders);
 
 export default orderRouter;
