009760f9a492420d1210fe2807557ab01fc25c11
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-09-05 16:15:36 +0800

    wip(FavoriteSchema.ts): add favorite schema and other related schema.

---
+++ src/schemas/FavoriteSchema.ts
@@ -0,0 +1,15 @@
+import z from "zod";
+
+export const FavoriteSchema = z.object({
+  id: z.number().nonnegative(),
+  user_id: z.string(),
+  product_id: z.number().nonnegative(),
+  created_at: z.string(),
+});
+
+export const AddToFavoriteSchema = FavoriteSchema.pick({
+  user_id: true,
+  product_id: true,
+});
+
+export type AddToFavorite = z.infer<typeof AddToFavoriteSchema>;
